448b63bcd146df9d12726e52abaf96bc896ddf0f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-11-23 12:27:13 +0600

    add gitignore file

--- getting_started.py
+++ getting_started.py
@@ -1,10 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+import argparse
+# driver = webdriver.Firefox()
+# driver.get ("https://dashboard.getalice.ai/")
+# driver.find_element_by_id("userName").send_keys("test.misfit")
+# driver.find_element_by_id("password").send_keys('qwerty123456')
 
-driver = webdriver.Firefox()
-driver.get ("https://dashboard.getalice.ai/")
-driver.find_element_by_id("userName").send_keys("test.misfit")
-driver.find_element_by_id("password").send_keys('qwerty123456')
+# login_btn = driver.find_element_by_class_name('login-button-round')
+# driver.execute_script("arguments[0].click();", login_btn)
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--name')
+   parser.add_argument('--age')
+   args = parser.parse_args()
 
-login_btn = driver.find_element_by_class_name('login-button-round')
-driver.execute_script("arguments[0].click();", login_btn)
+   print(args.name)
+   print(args.age)
+
+   my_dict = {'arg1': args.name, 'arg2': args.age }
+   print(my_dict)
