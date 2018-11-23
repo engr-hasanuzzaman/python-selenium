86896883f474b344152613c22f20d0c084944133
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-11-23 12:41:52 +0600

    complete basic login flow for alice login

--- getting_started.py
+++ getting_started.py
@@ -1,21 +1,30 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import argparse
-# driver = webdriver.Firefox()
-# driver.get ("https://dashboard.getalice.ai/")
-# driver.find_element_by_id("userName").send_keys("test.misfit")
-# driver.find_element_by_id("password").send_keys('qwerty123456')
 
-# login_btn = driver.find_element_by_class_name('login-button-round')
-# driver.execute_script("arguments[0].click();", login_btn)
+# contain username, password for login
+username = None
+password = None
+# TODO: it should come from arg
+target_url = "https://dashboard.getalice.ai/" 
+
 if __name__ == '__main__':
    parser = argparse.ArgumentParser()
-   parser.add_argument('--name')
-   parser.add_argument('--age')
+   parser.add_argument('--username')
+   parser.add_argument('--password')
    args = parser.parse_args()
+   username = args.username
+   password = args.password 
 
-   print(args.name)
-   print(args.age)
+if username is None or password is None:
+  print('---- please provide login credential for sign-in')
+  print('usage: python filename --usernam username --password password')
+  exit()
 
-   my_dict = {'arg1': args.name, 'arg2': args.age }
-   print(my_dict)
+driver = webdriver.Firefox()
+driver.get (target_url)
+driver.find_element_by_id("userName").send_keys(username)
+driver.find_element_by_id("password").send_keys(password)
+
+login_btn = driver.find_element_by_class_name('login-button-round')
+driver.execute_script("arguments[0].click();", login_btn)
